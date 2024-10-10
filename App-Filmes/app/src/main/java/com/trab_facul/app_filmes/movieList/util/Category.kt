3bc0725a5879163430972169a3ba0059485f6beb
Gustavo Horning <121242572+GustavoHorning@users.noreply.github.com>
2024-10-09 21:23:10 -0300

    UtilsMovieList

---
+++ App-Filmes/app/src/main/java/com/trab_facul/app_filmes/movieList/util/Category.kt
@@ -0,0 +1,6 @@
+package com.trab_facul.app_filmes.movieList.util
+
+object Category {
+    const val POPULAR = "popular"
+    const val UPCOMING = "upcoming"
+}
---
+++ App-Filmes/app/src/main/java/com/trab_facul/app_filmes/movieList/util/Screen.kt
@@ -0,0 +1,8 @@
+package com.trab_facul.app_filmes.movieList.util
+
+sealed class Screen(val rout: String) {
+    object Home : Screen("main")
+    object PopularMovieList : Screen("popularMovie")
+    object UpcomingMovieList : Screen("upcomingMovie")
+    object Details : Screen("details")
+}
